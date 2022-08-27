2503a6d4fe4426ba4079bf8b0bfcba84391b0640
FrancoTalles <francotallesextreme@gmail.com>
2022-08-27 00:15:26 -0400

    Manter Conexao feito

--- Js/script.js
+++ Js/script.js
@@ -1,10 +1,12 @@
 let nome = prompt("Qual é o seu nome?");
+let CancelaTempo;
 EntrarNaSala();
 function EntrarNaSala(){
     let objnome = {name: nome};
     const envioNome = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", objnome);
     envioNome.then(tratarSucessoNome);
     envioNome.catch(tratarErroNome);
+    CancelaTempo = setInterval(StayOnline, 5000);
 }
 
 function tratarSucessoNome(resposta){
@@ -16,3 +18,18 @@ function tratarErroNome(resposta){
     EntrarNaSala();
 }
 
+function StayOnline(){
+    let objnomeonline = {name: nome};
+    const envioOnline = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", objnomeonline);
+    envioOnline.then(tratarSucessoOnline);
+    envioOnline.catch(tratarErroOnline);
+}
+
+function tratarSucessoOnline(resposta){
+    console.log(resposta);
+}
+
+function tratarErroOnline(resposta){
+    console.log(resposta);
+    clearInterval(CancelaTempo);
+}
