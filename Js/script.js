aa094331cce91f1028d23ccbdd1fcbc9a1707920
FrancoTalles <francotallesextreme@gmail.com>
2022-08-27 01:42:17 -0400

    Mensagens, suas diferenciaçoes, seu carregamento e rolagem feitos

--- Js/script.js
+++ Js/script.js
@@ -1,5 +1,6 @@
 let nome = prompt("Qual é o seu nome?");
 let CancelaTempo;
+let MensagensCarrega;
 EntrarNaSala();
 function EntrarNaSala(){
     let objnome = {name: nome};
@@ -7,6 +8,7 @@ function EntrarNaSala(){
     envioNome.then(tratarSucessoNome);
     envioNome.catch(tratarErroNome);
     CancelaTempo = setInterval(StayOnline, 5000);
+    MensagensCarrega = setInterval(ListaMensagens, 3000);
 }
 
 function tratarSucessoNome(resposta){
@@ -32,4 +34,49 @@ function tratarSucessoOnline(resposta){
 function tratarErroOnline(resposta){
     console.log(resposta);
     clearInterval(CancelaTempo);
+    clearInterval(MensagensCarrega);
+}
+
+function ListaMensagens(){
+    const mensagens = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
+    mensagens.then(processaSucessoResposta);
+    mensagens.catch(processaErroResposta);
+    
+}
+
+function processaSucessoResposta(resposta){
+    let ul = document.querySelector(".mensagens");
+    for (let i =0; i < resposta.data.length; i++){
+        if (resposta.data[i].type === "status"){
+            ul.innerHTML += 
+            `<li class="status">
+                <span class="horario">(${resposta.data[i].time})</span>
+                <span class="nome">${resposta.data[i].from}</span> ${resposta.data[i].text}
+            </li>`;
+            const elementoQueQueroQueApareca = document.querySelector('.status:last-child');
+            elementoQueQueroQueApareca.scrollIntoView();
+        }else if (resposta.data[i].type === "message"){
+            ul.innerHTML += 
+            `<li class="mensagem">
+                <span class="horario">(${resposta.data[i].time})</span>
+                <span class="nome">${resposta.data[i].from}</span> para <span class="nome">${resposta.data[i].to}:</span> ${resposta.data[i].text}
+            </li>`;
+            const elementoQueQueroQueApareca = document.querySelector('.mensagem:last-child');
+            elementoQueQueroQueApareca.scrollIntoView();
+        }else if (resposta.data[i].type === "private_message" && resposta.data[i].to === nome){
+             ul.innerHTML += 
+             `<li class="privada">
+                 <span class="horario">(${resposta.data[i].time})</span>
+                 <span class="nome">${resposta.data[i].from}</span> para <span class="nome">${resposta.data[i].to}:</span> ${resposta.data[i].text}
+                 João :)
+             </li>`;
+            const elementoQueQueroQueApareca = document.querySelector('.privada:last-child');
+            elementoQueQueroQueApareca.scrollIntoView();
+        }
+    }
+
+}
+
+function processaErroResposta(resposta){
+    console.log("Deu erro");
 }
